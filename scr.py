a65d1547adc4000c844e38973f4825958289fa08
Sergey Egorkin <serezhs.e@gmail.com>
2022-06-20 19:42:18 +0300

    добавил логирование

--- scr.py
+++ scr.py
@@ -1,10 +1,22 @@
+import logging
 import requests
 from bs4 import BeautifulSoup
 import telebot
 from telebot import types
 import random
+from dotenv import load_dotenv
+import os
 
-bot = telebot.TeleBot("2146011655:AAHeP6vc2PAG4HK0L-V4B3sS8RkcB1V5t3w")
+# конфигурация логирования в боте
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.ERROR, filename='bot_logs.log', filemode='w')
+
+# загрузка и получение переменных из .env файлаъ
+load_dotenv()
+bot_token = os.getenv('TOKEN')
+
+bot = telebot.TeleBot(bot_token)  # API-токен бота в телеграм
 
 
 # запуск бота
@@ -28,69 +40,78 @@ def message_processing(message):
         photo = get_photo(url)
 
         bot.send_photo(message.chat.id, photo)
-    # если jnghfdbnm скриншот не удалось -
+
+    # если отправить скриншот не удалось -
     # пользователю выводится сообщение об ошибке
-    except Exception:  # as error:
-        # logging.error("Ошибка при получении скриншота")
+    except Exception as error:
+        logging.error(f'ошибка при отправке скриншота: {error}')
         bot.send_message(message.chat.id, "error")
 
 
-# генерация случайно ссылки со скриншотом
+# генерация случайной ссылки со скриншотом
 def generate_url():
-    letters = [
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "w",
-        "x",
-        "y",
-        "z",
-    ]
-    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    url = "https://prnt.sc/"
-    return (
-        url
-        + random.choice(letters)
-        + random.choice(letters)
-        + random.choice(numbers)
-        + random.choice(numbers)
-        + random.choice(numbers)
-        + random.choice(numbers)
-    )
+    try:
+        letters = [
+            "a",
+            "b",
+            "c",
+            "d",
+            "e",
+            "f",
+            "g",
+            "h",
+            "i",
+            "j",
+            "k",
+            "l",
+            "m",
+            "n",
+            "o",
+            "p",
+            "q",
+            "r",
+            "s",
+            "t",
+            "u",
+            "v",
+            "w",
+            "x",
+            "y",
+            "z",
+        ]
+        numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        url = "https://prnt.sc/"
+        return (
+            url
+            + random.choice(letters)
+            + random.choice(letters)
+            + random.choice(numbers)
+            + random.choice(numbers)
+            + random.choice(numbers)
+            + random.choice(numbers)
+        )
+
+    except Exception as error:
+        logging.error(f'ошибка при генерации ссылки: {error}')
 
 
 # получение фото с сайта
 def get_photo(url):
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 YaBrowser/19.10.2.195 Yowser/2.5 Safari/537.36"
-    }
+    try:
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 YaBrowser/19.10.2.195 Yowser/2.5 Safari/537.36"
+        }
 
-    html = requests.get(url, headers=headers).text
-    soup = BeautifulSoup(html, "lxml")
+        html = requests.get(url, headers=headers).text
+        soup = BeautifulSoup(html, "lxml")
 
-    img_tag = soup.find("img", {"id": "screenshot-image"})
-    photo_url = img_tag["src"]
+        img_tag = soup.find("img", {"id": "screenshot-image"})
+        photo_url = img_tag["src"]
 
-    return photo_url
+        return photo_url
+
+    except Exception as error:
+        logging.error(f'ошибка при получении фото с сайта: {error}')
 
 
 def main():
